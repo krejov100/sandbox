7f4621763fd2451266b0de50468cd46ecaa5e29c
philip krejov <krejov100@msn.com>
2018-01-10 21:23:52 -0800

    added string_util and vector_util

---
+++ cpp/vector_util.hpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+namespace pk {
+
+    template<typename T>
+    std::vector<std::vector<T> > transpose(const std::vector<std::vector<T> > &data) {
+        // https://stackoverflow.com/questions/6009782/how-to-pivot-a-vector-of-vectors
+        // this assumes that all inner vectors have the same size and
+        // allocates space for the complete result in advance
+        std::vector<std::vector<T> > result(data[0].size(),
+            std::vector<T>(data.size()));
+        for (std::vector<T>::size_type i = 0; i < data[0].size(); i++)
+            for (std::vector<T>::size_type j = 0; j < data.size(); j++) {
+                result[i][j] = data[j][i];
+            }
+        return result;
+    }
+
+}
