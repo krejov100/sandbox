b6262ee471a2e62af719685052c00aa7aa01e7e8
philip_Nutter <krejov100@msn.com>
2018-01-14 19:37:54 -0800

    ctest seems to be working

--- cpp/tests/tests.cpp
+++ cpp/tests/tests.cpp
@@ -1,13 +1,4 @@
-#define DOCTEST_CONFIG_IMPLEMENT
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
 #include "../src/sandbox.hpp"
-
-int main(int argc, char** argv)
-{ 
-    doctest::Context context;
-	context.setOption("no-breaks", true);
-    int res = context.run();
-	if (context.shouldExit()) // important - query flags (and --exit) rely on the user doing this
-		return res;          // propagate the result of the tests	
-}
