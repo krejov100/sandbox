5db71f6691394e2fed5a163f008ea266acfdff30
philip_Nutter <krejov100@msn.com>
2018-01-14 22:07:44 -0800

    added tests to histogram.hpp

--- cpp/src/statistics/histogram.hpp
+++ cpp/src/statistics/histogram.hpp
@@ -1,22 +1,6 @@
 #include <vector>
 #include "range.hpp"
-
-#ifdef DOCTEST_LIBRARY_INCLUDED
-TEST_CASE("quick test 1") {
-    CHECK(2 == 2);
-}
-
-TEST_CASE("quick test 2")
-{
-    CHECK(2 == 2);
-}
-
-TEST_CASE("quick test 3")
-{
-    CHECK(2 == 3);
-}
-#endif DOCTEST_LIBRARY_INCLUDED
-
+#include <numeric>
 
 template<typename T>
 struct Histogram
@@ -25,7 +9,6 @@ struct Histogram
         Range<T> range;
         int count;
     };
-
     SparseVector<Bin> bins; // indexed bins
 };
 
@@ -47,6 +30,22 @@ inline std::pair<size_t, Range<float>> linear_quantize(const float &value, size_
     return std::make_pair(stepId, Range<float>{lowerPoint, upperPoint});
 }
 
+#ifdef DOCTEST_LIBRARY_INCLUDED
+TEST_CASE("linear_quantize test")
+{
+	auto rslt = linear_quantize(0.234f, 4, Range<float>{-20.0f, 20.0f});
+	CHECK(rslt.first == 2);
+	CHECK(rslt.second.min == doctest::Approx(0.0f));
+	CHECK(rslt.second.max == doctest::Approx(10.0f));
+	
+	rslt = linear_quantize(0.234f, 4, Range<float>{0, 20.0f});
+	CHECK(rslt.first == 0);
+	CHECK(rslt.second.min == doctest::Approx(0.0f));
+	CHECK(rslt.second.max == doctest::Approx(5.0f));
+}
+#endif DOCTEST_LIBRARY_INCLUDED
+
+
 template<typename T, typename F>
 inline Histogram<T> compute_histogram(const std::vector<T> &values, size_t nBins, const Range<T> &minMax, F&& biningFunctor)
 {
@@ -63,3 +62,17 @@ inline Histogram<T> compute_histogram(const std::vector<T> &values, size_t nBins
     }
     return rslt;
 }
+
+#ifdef DOCTEST_LIBRARY_INCLUDED
+TEST_CASE("compute_histogram test")
+{
+	std::vector<float> values = { 0.234f,12.45f, 9.324f, -3.34f, 8.0f, -2.98f, -20.0f, 0.0f };
+	auto hist = compute_histogram(values, 4, Range<float>{-20.0f, 20.0f}, linear_quantize);
+	const size_t totalCount = std::accumulate(hist.bins.begin(), hist.bins.end(), 0, [](const size_t previous, auto& p) { return previous + p.second.count; });
+	
+	CHECK(hist.bins.size() == 4);
+	CHECK(totalCount == values.size());
+	CHECK(hist.bins[0].range.min == doctest::Approx(-20.0f));
+	CHECK(hist.bins[0].range.max == doctest::Approx(-10.0f));
+}
+#endif DOCTEST_LIBRARY_INCLUDED
