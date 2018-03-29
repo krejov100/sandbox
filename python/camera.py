1ef7281f2b86f2e0d718c8f511c91ae13e884bf0
krejov100 <krejov100@msn.com>
2018-03-30 00:06:48 +0100

    Adding Pinhole Camera

---
+++ python/camera.py
@@ -0,0 +1,27 @@
+import numpy as np
+class ImagePoint:
+    x = 0
+    y = 0
+
+
+class FocalLength:
+    fx = 0
+    fy = 0
+
+
+class PinHoleCamera:
+    principlePoint = ImagePoint
+    focalLength = FocalLength
+    dimensions = ImagePoint
+
+class ViPiCamera:
+    camera = PinHoleCamera
+
+class Pose:
+    orientation = np.asarray([0,0,0,1])
+    position = np.zeros(3)
+
+class MultiCameraArray:
+    cameras = []
+    globalPoses = []
+
